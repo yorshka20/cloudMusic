ee344d367e5dbc8ebdf5d2f2b72efbb712fd16b6
yorshka <yorshka@hotmail.com>
2021-05-06 21:47:50 +0800

    feat: add eslint config.

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'standard',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+      modules: true,
+    },
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  settings: {
+    // 自动发现React的版本，从而进行规范react代码
+    react: {
+      pragma: 'React',
+      version: 'detect',
+    },
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+  },
+};
