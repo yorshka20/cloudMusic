3e125d54ba379a97d6eea893b9aa5621dfa4b22e
yorshka <yorshka@youme.im>
2021-05-06 22:09:37 +0800

    fix: eslint config fix.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,29 +4,33 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'standard',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    "standard",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
   ],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
       modules: true,
     },
     ecmaVersion: 2021,
-    sourceType: 'module',
+    sourceType: "module",
   },
   settings: {
     // 自动发现React的版本，从而进行规范react代码
     react: {
-      pragma: 'React',
-      version: 'detect',
+      pragma: "React",
+      version: "detect",
     },
   },
-  plugins: ['react', '@typescript-eslint', 'prettier'],
+  plugins: ["react", "@typescript-eslint", "prettier"],
   rules: {
-    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    "react/jsx-filename-extension": [
+      2,
+      { extensions: [".js", ".jsx", ".ts", ".tsx"] },
+    ],
+    "no-use-before-define": "off",
   },
 };
